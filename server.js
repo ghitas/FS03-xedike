09e426747843c4b641ef4c4adeecafe8afdf45fb
pcthanh <caothanhphung2237@gmail.com>
2019-04-12 20:35:45 +0700

    create user models

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 /** 3rd party package */
 const express = require('express');
+const mongoose = require('mongoose');
 
 /** project package */
+mongoose
+  .connect('mongodb://localhost:27017/fs03-xedike', { useNewUrlParser: true })
+  .then(console.log('connected to dadabase'))
+  .catch(console.log);
 
 /** initialize server */
 const app = express();
 const port = process.env.PORT || 5000; //env -> invironment
 app.listen(port, () => {
-  console.log('Connect to DB on ' + port);
+  console.log('Server running on port: ' + port);
 });
