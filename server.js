c9eefc2f443a2ff429ff8d8cc81120888e5e05dc
pcthanh <caothanhphung2237@gmail.com>
2019-04-12 19:53:29 +0700

    initialize server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+/** 3rd party package */
+const express = require('express');
+
+/** project package */
+
+/** initialize server */
+const app = express();
+const port = process.env.PORT || 5000; //env -> invironment
+app.listen(port, () => {
+  console.log('Connect to DB on ' + port);
+});
